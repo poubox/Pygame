dc5938adafd9e1ae4069fd4bb0656e595ccf4e25
Young <gundolzang@naver.com>
2024-07-02 20:11:45 +0900

    first commit

---
+++ pygame_basic/1_create_frame.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init() # 초기화 (반드시 필요)
+
+# 화면 크기 설정
+screen_width = 480 # 가로 크기
+screen_height = 640 # 세로 크기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("Nado Game") # 게임 이름
+
+# 이벤트 루프
+running = True # 게임이 진행중인가?
+while running:
+    for event in pygame.event.get(): # 어떤 이벤트가 발생하였는가? pygame을 진행하기 위한 필수 구문
+        if event.type == pygame.QUIT: # 창을 마우스로 닫을 때
+            running = False # 게임이 진행중이 아님
+
+
+
